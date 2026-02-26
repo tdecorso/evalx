ea59f56b041b118840182ad64445a7d97ce1d5e1
tdecorso <tony.dcorso@gmail.com>
2026-02-26 21:44:20 +0100

    Getting input buffer

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
-
-#include "tds/stack.h"
+#include <string.h>
 
 int main(void) {
-    printf("Hello world.\n");
+    char buffer[256];
+    
+    printf(" evalx v0.1 ('q' to quit) \n");
+
+    while (1) {
+        printf("  > ");
+        if (!fgets(buffer, sizeof(buffer), stdin)) break;
+        buffer[strcspn(buffer, "\n")] = 0;
+        if (strcmp(buffer, "q") == 0) break;
+
+        printf("You typed: %s\n", buffer);
+    }
     return 0;
 }
